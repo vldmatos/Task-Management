9de5b70ac0bc94115e8365a1b392a959de66fb8e
Vladimir Matos <vladimir.matos@outlook.com>
2025-04-17 22:08:39 -0300

    [feat]: Create endpoints Projects and Tasks
    - Refactored `Projects.cs` to use async methods for endpoints.
    - Changed `Group` property from "projects" to "Projects".
    - Added endpoints for retrieving projects and tasks by ID.
    - Updated `MapPost` for project creation to return a Created result.
    - Created `Tasks.cs` for task management with update and delete endpoints.
    
    Issue: #4, #3, #5, #6, #7
    changes made by: Vladimir Matos

--- source/API/Endpoints/Projects.cs
+++ source/API/Endpoints/Projects.cs
@@ -1,30 +1,73 @@
 ﻿using Configurations.Data;
 using Configurations.Extensions;
 using Domain;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Endpoints;
 
 public class Projects : IEndpoint
 {
-    public string Group => "projects";
+    public string Group => "Projects";
 
     public void MapEndpoints(IEndpointRouteBuilder endpointRouteBuilder)
     {
-        endpointRouteBuilder.MapGet
-        ("/projects", () =>
 
-            "Projects!"
-        )
-        .WithTags(Group)
-        .WithOpenApi();
-
-        endpointRouteBuilder.MapPost("/projects", async (DataContext dataContext, Project newProject) =>
+        endpointRouteBuilder.MapGet("/projects",
+            async Task<Ok<Project[]>>
+            (DataContext dataContext) =>
         {
-            dataContext.Projects.Add(newProject);
-            await dataContext.SaveChangesAsync();
-            return Results.Created($"/projects/{newProject.Id}", newProject);
+            var projects = await dataContext.Projects
+                                            .AsNoTracking()
+                                            .ToArrayAsync();
+
+            return TypedResults.Ok(projects);
         })
-        .WithTags(Group)
-        .WithOpenApi();
+        .WithDescription("Get all projects")
+        .WithTags(Group);
+
+
+        endpointRouteBuilder.MapGet("/projects/{id}",
+            async Task<Results<Ok<Project>, NotFound>>
+            (int id, DataContext dataContext) =>
+        {
+            var project = await dataContext.Projects
+                                           .AsNoTracking()
+                                           .FirstOrDefaultAsync(item => item.Id == id);
+
+            return project is not null ?
+                TypedResults.Ok(project) :
+                TypedResults.NotFound();
+        })
+        .WithDescription("Get project by id")
+        .WithTags(Group);
+
+
+        endpointRouteBuilder.MapGet("/projects/{id}/tasks",
+            async Task<Results<Ok<TaskProject>, NotFound>>
+            (int id, DataContext dataContext) =>
+        {
+            var tasks = await dataContext.Tasks
+                                           .AsNoTracking()
+                                           .FirstOrDefaultAsync(item => item.ProjectId == id);
+
+            return tasks is not null ?
+                TypedResults.Ok(tasks) :
+                TypedResults.NotFound();
+        })
+        .WithDescription("Get tasks by project id")
+        .WithTags(Group);
+
+
+        endpointRouteBuilder.MapPost("/projects",
+            async (DataContext dataContext, Project project) =>
+        {
+            dataContext.Projects.Add(project);
+            await dataContext.SaveChangesAsync();
+
+            return TypedResults.Created();
+        })
+        .WithDescription("Create a new project")
+        .WithTags(Group);
     }
 }
---
+++ source/API/Endpoints/Tasks.cs
@@ -0,0 +1,48 @@
+﻿using Configurations.Data;
+using Configurations.Extensions;
+using Domain;
+
+namespace API.Endpoints;
+
+public class Tasks : IEndpoint
+{
+    public string Group => "Tasks";
+
+    public void MapEndpoints(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapPut("/tasks/{id}",
+            async (int id, TaskProject taskProject, DataContext dataContext) =>
+        {
+            var registeredTask = await dataContext.Tasks.FindAsync(id);
+            if (registeredTask is null)
+                return Results.NotFound();
+
+            registeredTask.Title = taskProject.Title ?? registeredTask.Title;
+            registeredTask.Description = taskProject.Description ?? registeredTask.Description;
+            registeredTask.Status = taskProject.Status ?? registeredTask.Status;
+            registeredTask.DueDate = taskProject.DueDate;
+
+            await dataContext.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithDescription("Update task by id")
+        .WithTags(Group);
+
+
+        endpointRouteBuilder.MapDelete("/tasks/{id}",
+            async (int id, DataContext dataContext) =>
+        {
+            var task = await dataContext.Tasks.FindAsync(id);
+            if (task is null)
+                return Results.NotFound();
+
+            dataContext.Tasks.Remove(task);
+            await dataContext.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithDescription("Delete task by id")
+        .WithTags(Group);
+    }
+}
